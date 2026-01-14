997dd4fb8cb838da919ad926a9ffe2dfe62ce2dc
siljakluge <silja.kluge@outlook.de>
2026-01-14 13:20:41 +0100

    update

--- CybORG/Simulator/State.py
+++ CybORG/Simulator/State.py
@@ -454,7 +454,7 @@ class State(CybORGLogger):
         host.sessions = {}
         host.processes = []
         for file in host.files:
-            if file.path == "/evaluation_contractor_active_-5/":
+            if file.path == "/evaluation_contractor_active_0/":
                 host.files.remove(file)
 
         # start back up
